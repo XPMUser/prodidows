e01f90857e2d7b6a0ba556162b2f935c68ea6448
Toonigy <62924215+Toonigy@users.noreply.github.com>
2024-08-13 18:14:47 -0700

    Create google.js

---
+++ 1-50-0/js/google.js
@@ -0,0 +1,14 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLTCdWLmOfE_QrR13Z15VjRn2gTh4BvPo",
+  authDomain: "triple-bird-410810.firebaseapp.com",
+  databaseURL: "https://triple-bird-410810-default-rtdb.firebaseio.com",
+  projectId: "triple-bird-410810",
+  storageBucket: "triple-bird-410810.appspot.com",
+  messagingSenderId: "220442635630",
+  appId: "1:220442635630:web:87ea71a698d7195c3d657f",
+  measurementId: "G-3LYT0H9SXG"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
